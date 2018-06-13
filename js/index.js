5effc69a5c2ae55e5e23d8671a8782cc41fad002
inggabo001 <gabojunior05@gmail.com>
2018-06-13 18:35:11 -0400

    First commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+/*
+	** Funciones de la pagina principal **
+
+	Cargar los cursos de Javascript y JQuery
+*/
+
+
+function jsCourse() {
+	location.href ="js-course.html";
+}
+
+function jqCourse(){
+	location.href ="jq-course.html";
+}
---
+++ js/js-course.js
@@ -0,0 +1,6 @@
+// funciones para la pantalla de los cursos de javascript
+
+function unidad1() {
+	location.href ="js-intro.html";
+}
+
